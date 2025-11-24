1882c83a7856afba2152ea4780757a403f56b10a
Riy65 <riyarpit71325@gmail.com>
2025-11-24 22:35:46 +0530

    Time: 8 ms (47.37%) | Memory: 13.6 MB (37.01%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        slow= 0
+        for fast in range(len(nums)):
+            if nums[fast]!=0:
+                nums[slow]=nums[fast]
+                slow+=1
+        while slow< len(nums):
+            nums[slow]=0
+            slow+=1
+        return nums
+
+     
+        
