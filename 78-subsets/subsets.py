c98923b9852fd7f1922ff989f7d41391035fb8c3
Riy65 <riyarpit71325@gmail.com>
2026-06-30 19:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (56.41%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,22 +1,14 @@
 class Solution(object):
     def subsets(self, nums):
         ans = []
-
         def solve(index, subset):
             if index == len(nums):
                 ans.append(subset[:])
                 return
-
-            # Take
             subset.append(nums[index])
             solve(index + 1, subset)
-
-            # Backtrack
             subset.pop()
-
-            # Not Take
             solve(index + 1, subset)
-
         solve(0, [])
         return ans
      
